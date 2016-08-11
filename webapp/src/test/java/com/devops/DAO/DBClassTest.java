d0beff81f593ef45284d5b3ad5732db20b0c8be0
Enteez <enteez.git@gmail.com>
2016-08-11 15:10:42 +0530

    updated test cases

--- webapp/src/test/java/com/devops/DAO/DBClassTest.java
+++ webapp/src/test/java/com/devops/DAO/DBClassTest.java
@@ -1,29 +1,34 @@
 import com.devops.DAO.DBClass;
-
 import junit.framework.TestCase; 
+import com.devops.BO.Employee;
 
 public class DBClassTest extends TestCase {
-
 	public void testDBClassConstructor()
     {
 		DBClass dc = new DBClass("10.244.54.78","3306","devopsdb","root","Pspl@123");
 		dc.closeConnection();
-		assertTrue( dc != null );
+		assertTrue( dc != null);
     }
 	
 	public void testDBClassAuthenticateCorrect()
     {
 		DBClass dc = new DBClass("10.244.54.78","3306","devopsdb","root","Pspl@123");
 		int ret = dc.authenticate(11560, "12345");
-		assertTrue( ret == 1 );
+		assertTrue( ret == 1);
     }
 	
 	public void testDBClassAuthenticateInCorrect()
     {
 		DBClass dc = new DBClass("10.244.54.78","3306","devopsdb","root","Pspl@123");
 		int ret = dc.authenticate(0, "");
-		assertTrue( ret != 1 );
+		assertTrue( ret != 1);
     }
 	
+	public void testgetLateStayList()
+	{
+		DBClass dc = new DBClass("10.244.54.78","3306","devopsdb","root","Pspl@123");
+		Employee[] emp_list=dc.getLateStayList();
+		assertTrue(emp_list.length > 0);
+	}
 }
 
