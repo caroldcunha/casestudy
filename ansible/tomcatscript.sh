b7b99354840a04a0552a4e230e51928e5bccc4ac
root <carol_dcunha@persistent.com>
2016-08-13 22:48:12 +0530

    Updated Tomcat playbook and script

--- ansible/tomcatscript.sh
+++ ansible/tomcatscript.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-echo "export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64" >> ~/.bashrc
-echo "export CATALINA_HOME=/opt/apache-tomcat-6.0.32" >> ~/.bashrc
+str1=`cat ~/.bashrc | grep '^export JAVA_HOME'`
+str2=`cat ~/.bashrc | grep '^export CATALINA_HOME'`
+
+if [ -z "$str1" ]
+then
+   echo "export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64" >> ~/.bashrc
+fi
+if [ -z "$str2" ]
+then
+   echo "export CATALINA_HOME=/opt/apache-tomcat-6.0.32" >> ~/.bashrc
+fi
