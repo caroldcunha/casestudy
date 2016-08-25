8d67146520446e9ca2b081bde4eba6319787d36a
carol_dcunha <carol_dcunha@persistent.co.in>
2016-08-25 18:12:02 +0530

    updated files

--- ansible/clusterScripts/sqlscript.sh
+++ ansible/clusterScripts/sqlscript.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-cp -r mysql-cluster-gpl-7.4.12-linux-glibc2.5-x86_64 /usr/local/
+cp -r /root/mysql-cluster-gpl-7.4.12-linux-glibc2.5-x86_64 /usr/local/
 ln -s /usr/local/mysql-cluster-gpl-7.4.12-linux-glibc2.5-x86_64 /usr/local/mysql
 cd /usr/local/mysql
 scripts/mysql_install_db --user=mysql
 chown -R root .
 chown -R mysql data
 chgrp -R mysql .
+cp support-files/mysql.server /etc/init.d
+chmod +x /etc/init.d/mysql.server
+update-rc.d mysql.server defaults
